4d969f9112919aba1074db1130fa741f7ba6b9e3
Dima Gonikman <dima.gonikman@gmail.com>
2021-03-23 14:54:35 +0200

    Add multifile + output

--- runner.py
+++ runner.py
@@ -11,25 +11,29 @@ def read_config():
 
 
 class Config:
-    def __init__(self, log_level: str = "ERROR", ignore_list=None, storage=None):
+    def __init__(self, log_level: str = "ERROR", ignore_list=None, storage=None, filenames=None):
         if ignore_list is None:
             ignore_list = []
         if storage is None:
             storage = ["console"]
+        if filenames is None:
+            storage = []
 
         self.log_level = log_level
         self.ignore_list = ignore_list
         self.storage = storage
+        self.filenames = filenames
 
 
 if __name__ == "__main__":
     config = read_config()
     logging.basicConfig(level=config.log_level)
-
-    c: Counter = Counter("alice.txt", config.ignore_list)
     storage_factory = StorageFactory()
 
-    for storage_type in config.storage:
-        c.add_exporter(storage_factory.produce(storage_type))
+    for filename in config.filenames:
+        c: Counter = Counter(filename, config.ignore_list)
 
-    c.get_top_k(5)
+        for storage_type in config.storage:
+            c.add_exporter(storage_factory.produce(storage_type))
+
+        c.get_top_k(100)
